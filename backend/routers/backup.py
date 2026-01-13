3a17f5b280b2e7bc3c06833d5c4e7efc088dc2d9
McMa3zeye <omar.makkouk2015@gmail.com>
2026-01-13 23:03:45 +0100

    Update backup.py

--- backend/routers/backup.py
+++ backend/routers/backup.py
@@ -7,7 +7,7 @@ from fastapi.responses import FileResponse
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import SQLAlchemyError
 from typing import Optional
-from datetime import datetime
+from datetime import datetime, timezone
 import os
 import shutil
 import json
